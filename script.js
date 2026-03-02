5c6b84a09e491a5d90b690aa73dd28c1a9d614a7
hasan-goljar-dev <hasangolaj.r@gmail.com>
2026-03-02 19:22:48 +0600

    Fix login validation bug

--- script.js
+++ script.js
@@ -1,91 +1,107 @@
 let currentTab = "all";
-const tabActive = ['bg-blue-800', 'border-navy', 'text-white'];
-const tebInactive = ['bg-transparent', 'text-slate-700', 'border-slate-700'];
+const tabActive = ["bg-blue-800", "border-navy", "text-white"];
+const tabInactive = ["bg-transparent", "text-state-700", "border-state-200"];
+
 
 const allContainer = document.getElementById("all-container");
 const interviewContainer = document.getElementById("interview-container");
 const rejectedContainer = document.getElementById("reject-container");
 const emptyState = document.getElementById("empty-state")
-
-
-
 function switchTab(tab) {
-
     const tabs = ["all", "interview", "rejected"];
+    currentTab = tab;
     for (const t of tabs) {
         const tabName = document.getElementById("tab-" + t);
         if (t === tab) {
-            tabName.classList.remove(...tebInactive);
+            tabName.classList.remove(...tabInactive);
             tabName.classList.add(...tabActive);
         }
         else {
             tabName.classList.remove(...tabActive);
-            tabName.classList.add(...tebInactive);
+            tabName.classList.add(...tabInactive)
         }
     }
-    const pages = [allContainer, interviewContainer, rejectedContainer];
-    for (const section of pages) {
+    const allPart = [allContainer, interviewContainer, rejectedContainer]
+    for (const section of allPart) {
         section.classList.add("hidden");
     }
-}
-    // emptyState.classList.add("hidden");
+    emptyState.classList.add("hidden")
     if (tab === "all") {
         allContainer.classList.remove("hidden");
         if (allContainer.children.length < 1) {
             emptyState.classList.remove("hidden")
-
         }
-        else if (tab === "interview") {
-            interviewContainer.classList.remove("hidden");
-            if (interviewContainer.children.length < 1) {
-                emptyState.classList.remove("hidden")
-            }
 
+    }
+    else if (tab === "interview") {
+        interviewContainer.classList.remove("hidden");
+        if (interviewContainer.children.length < 1) {
+            emptyState.classList.remove("hidden")
         }
-        else {
-            rejectedContainer.classList.remove("hidden");
-            if (rejectedContainer.children.length < 1) {
-                emptyState.classList.remove("hidden")
-            }
+    }
+    else {
+        rejectedContainer.classList.remove("hidden");
+        if (rejectedContainer.children.length < 1) {
+            emptyState.classList.remove("hidden")
         }
+    }
+    updateState();
+}
+
+// state Update
+const totalStat = document.getElementById("state-total");
+const interviewState = document.getElementById("state-interview");
+const rejectedState = document.getElementById("state-rejected");
+const Update = document.querySelectorAll(".state-applied");
+const availableState = document.getElementById("available");
+
+totalStat.innerText = allContainer.children.length;
+
+
+
+
+
+switchTab(currentTab);
+document.getElementById("jobs-container")
+    .addEventListener("click", function (event) {
+        const clickedElement = event.target;
+        const card = clickedElement.closest(".card");
+        const parent = card.parentNode;
+        if (clickedElement.classList.contains("interview")) {
+            // Update.classList = "badge badge-soft badge-success mb-2"
+            Update.innerText = "Interview"
+            interviewContainer.appendChild(card);
+            updateState()
         }
-        //state update
-        const totalState = document.getElementById("state-all");
-        const interviewState = document.getElementById("state-interview");
-        const rejectedSate = document.getElementById("state-rejected");
-        totalState.innerText = allContainer.children.length;
-        console.log(totalState)
-
-
-        switchTab(currentTab);
-
-        // all container
-        document.getElementById("jobs-container").addEventListener("click", function (event) {
-            const clickElement = event.target;
-            const card = clickElement.closest(".card");
-            const status = card.querySelector(".state-applied")
-            const parent = card.parentNode;
-            if (clickElement.classList.contains("interview")) {
-                status.classList.add("bg-green")
-                status.innerText = "Interview "
-                interviewContainer.appendChild(card)
-                updateState()
-            }
-            if (clickElement.classList.contains("rejected")) {
-                status.classList.add("bg-green")
-                status.innerText = "Rejected"
-                rejectedContainer.appendChild(card)
-                updateState()
-            }
-            if (clickElement.classList.contains("delete-btn")) {
-                parent.removeChild(card)
-                updateState()
-            }
-
-        })
-        function updateState() {
-            totalState.innerText = allContainer.children.length;
-            interviewState.innerText = interviewContainer.children.length;
-            rejectedSate.innerText = rejectedContainer.children.length;
+        if (clickedElement.classList.contains("rejected")) {
+            // Update.classList = "badge badge-soft badge-error mb-2"
+            Update.innerText = "Rejected"
+            rejectedContainer.appendChild(card);
+            updateState()
         }
-        updateState()
+        if (clickedElement.classList.contains("delete-btn")) {
+            parent.removeChild(card);
+            updateState()
+        }
+
+    });
+function updateState() {
+
+    const counts = {
+        all: allContainer.children.length,
+        interview: interviewContainer.children.length,
+        rejected: rejectedContainer.children.length
+    };
+    totalStat.innerText = counts.all;
+    interviewState.innerText = counts.interview;
+    rejectedState.innerText = counts.rejected;
+
+
+    availableState.innerText = counts[currentTab];
+    if (counts[currentTab] < 1) {
+        emptyState.classList.remove("hidden");
+    } else {
+        emptyState.classList.add("hidden");
+    }
+}
+updateState();
